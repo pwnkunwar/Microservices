a211187835387e87ee9550b317f1488d921d263c
pwn1kunwar <pwn1kunwar@gmail.com>
2024-10-29 10:56:10 +0545

    Implementing AutoMapper for Mapping Obj

--- Mango/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -1,4 +1,5 @@
-﻿using Mango.Services.CouponAPI.Data;
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
 using Mango.Services.CouponAPI.Models;
 using Mango.Services.CouponAPI.Models.Dtos;
 using Microsoft.AspNetCore.Mvc;
@@ -11,10 +12,13 @@ namespace Mango.Services.CouponAPI.Controllers
     {
         private readonly AppDbContext _db;
         private ResponseDto _response;
-        public CouponAPIController(AppDbContext db)
+        private IMapper _mapper;
+        public CouponAPIController(AppDbContext db,
+            IMapper mapper)
         {
             _db = db;   
             _response = new ResponseDto();
+            _mapper = mapper;
         }
         [HttpGet]
         public object Get()
@@ -22,7 +26,7 @@ namespace Mango.Services.CouponAPI.Controllers
             try
             {
                 IEnumerable<Coupon> objList = _db.Coupons.ToList();
-                _response.Result = objList;
+                _response.Result = _mapper.Map<IEnumerable<CouponDto>>(objList);
             }
             catch(Exception ex)
             {
@@ -38,7 +42,7 @@ namespace Mango.Services.CouponAPI.Controllers
             try
             {
                 Coupon obj = _db.Coupons.First(u => u.CouponId == id);
-                _response.Result = obj;
+                _response.Result = _mapper.Map<CouponDto>(obj);
             }
             catch(Exception ex)
             {
