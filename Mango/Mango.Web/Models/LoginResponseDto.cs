c3dc1e088e8814a36ae229e934ef77691491b9c9
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-22 22:26:00 +0545

    Add authentication service and models; update solution and project files

---
+++ Mango/Mango.Web/Models/LoginResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Services.Web.Models.Dtos
+{
+    public class LoginResponseDto
+    {
+        public UserDto User { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ Mango/Mango.Web/Service/TokenProvider.cs
@@ -0,0 +1,30 @@
+﻿using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+
+namespace Mango.Web.Service
+{
+    public class TokenProvider : ITokenProvider
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        public TokenProvider(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+        public void ClearToken()
+        {
+            _contextAccessor.HttpContext?.Response.Cookies.Delete(SD.TokenCookie);
+        }
+
+        public string? GetToken()
+        {
+            string? token = null;
+            bool? hasToken = _contextAccessor.HttpContext?.Request.Cookies.TryGetValue("token", out token);
+            return hasToken is true ? token : null;
+        }
+
+        public void SetToken(string token)
+        {
+            _contextAccessor.HttpContext?.Response.Cookies.Append(SD.TokenCookie, token);
+        }
+    }
+}
