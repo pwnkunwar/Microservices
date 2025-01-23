a115a719b013668717e73b31e98aa90ab95d037c
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-23 17:24:22 +0545

    set withBearer:false while calling API

--- Mango/Mango.Web/Service/AuthService.cs
+++ Mango/Mango.Web/Service/AuthService.cs
@@ -20,7 +20,7 @@ namespace Mango.Web.Service
                 ApiType = SD.ApiType.POST,
                 Data = registrationRequestDto,
                 Url = SD.AuthAPIBase + "/api/auth/AssignRole"
-            });
+            },withBearer:false);
 
 
         }
@@ -32,7 +32,7 @@ namespace Mango.Web.Service
                 ApiType = SD.ApiType.POST,
                 Data = loginRequestDto,
                 Url = SD.AuthAPIBase + "/api/auth/login"
-            });
+            }, withBearer: false);
         }
 
         public async Task<ResponseDto> RegisterAsync(RegistrationRequestDto registrationRequestDto)
@@ -42,7 +42,7 @@ namespace Mango.Web.Service
                 ApiType = SD.ApiType.POST,
                 Data = registrationRequestDto,
                 Url = SD.AuthAPIBase + "/api/auth/register"
-            });
+            }, withBearer: false);
         }
     }
 }
