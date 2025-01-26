95bc1bb5fd68b9c9d28552f69aa201cd38204087
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-26 16:13:10 +0545

    added ProductAPI Service

---
+++ Mango/Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Data;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private IMapper _mapper;
+        private ResponseDto _response;
+        public ProductAPIController(AppDbContext db,
+            IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+            _response = new ResponseDto();
+        }
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Product> objList = _db.Products.ToList();
+                _response.Result = _mapper.Map<IEnumerable<ProductDto>>(objList);
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+    }
+}
