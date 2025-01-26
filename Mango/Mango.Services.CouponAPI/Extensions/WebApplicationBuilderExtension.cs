e80f4416baeeb906e7a570eb67a84facec53bd12
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-26 16:13:41 +0545

    Extensions method added in CouponAPI service

---
+++ Mango/Mango.Services.CouponAPI/Extensions/WebApplicationBuilderExtension.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Mango.Services.CouponAPI.Extensions
+{
+    public static class WebApplicationBuilderExtension
+    {
+        public static WebApplicationBuilder AddAppAuthentication(this WebApplicationBuilder builder)
+        {
+            var settingsSection = builder.Configuration.GetSection("ApiSettings");
+            var secret = builder.Configuration.GetValue<string>("Secret");
+            var issuer = builder.Configuration.GetValue<string>("Issuer");
+            var audience = builder.Configuration.GetValue<string>("Audience");
+
+            var key = Encoding.ASCII.GetBytes(secret);
+
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience,
+                    ValidateAudience = true
+                };
+
+            });
+            return builder;
+        }
+    }
+}
