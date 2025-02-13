c2fe9a573d6d45cd9009ca5e08d62d7a2c848400
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-13 14:42:16 +0545

    interface ProductService added

---
+++ Mango/Mango.Web/Service/IService/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.Web;
+using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface IProductService
+    {
+        Task<ResponseDto?> GetAllProductAsync();
+        Task<ResponseDto?> GetProductByIdAsync(int id);
+        Task<ResponseDto?> CreateProductAsync(ProductDto couponDto);
+        Task<ResponseDto?> UpdateProductAsync(ProductDto couponDto);
+        Task<ResponseDto?> DeleteProductAsync(int id);
+    }
+}
