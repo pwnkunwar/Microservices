f4d1410531b316b4d1e1ffa96d219653c2fa3e67
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-16 08:29:20 +0545

    CartDetailsDto added

---
+++ Mango/Mango.Web/Models/CartDetailsDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.Web.Models.Dtos
+{
+    public class CartDetailsDto
+    {
+        public int CartDettailsId { get; set; }
+        public int CardHeaderId { get; set; }
+        public CartHeader? CartHeader { get; set; }
+        public int ProductId { get; set; }
+        public ProductDto? Product { get; set; }
+        public int Count { get; set; }
+    }
+}
