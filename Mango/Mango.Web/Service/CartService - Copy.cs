222a419cb0812211b0787282194175b10ec1772c
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-17 13:50:44 +0545

    code pushed

---
+++ Mango/Mango.Web/Service/CartService - Copy.cs
@@ -0,0 +1,59 @@
+﻿using Mango.Services.Web.Models;
+using Mango.Services.Web.Models.Dtos;
+using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using System.Security.Cryptography;
+
+namespace Mango.Web.Service
+{
+    public class CartService : ICartService
+    {
+        private readonly IBaseService _baseService;
+        public CartService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDto?> ApplyCouponAsync(CartDto cartDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDto,
+                Url = SD.CouponAPIBase + "/api/cart/ApplyCoupon"
+            });
+        }
+
+        public async Task<ResponseDto> GetCartByUserIdAsync(string userId)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/GetCart/" + userId
+            });
+        }
+
+        public async Task<ResponseDto?> RemoveFromCartAsync(int cartDetailsId)
+        {
+
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDetailsId,
+                Url = SD.CouponAPIBase + "/api/cart/RemoveCart"
+            });
+        }
+
+        public async Task<ResponseDto?> UpsertCartAsyc(CartDto cartDto)
+        {
+
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDto,
+                Url = SD.CouponAPIBase + "/api/cart/CartUpsert"
+            });
+        }
+    }
+}
